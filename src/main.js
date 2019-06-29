f87f35c5184c9fab616489d7f1ee1fd31c8b58e4
Zoexyf <1270355646@qq.com>
2019-06-29 09:54:09 +0800

    init my project

---
+++ src/main.js
@@ -0,0 +1,20 @@
+//入口文件
+// console.log('ok')
+import Vue from 'vue'
+
+import app from './App.vue'
+
+import './lib/mui/css/mui.min.css'
+
+import { Header } from 'mint-ui'
+
+
+
+Vue.component(Header.name, Header)
+
+var vm=new Vue({
+
+  el:'#app',
+  render: c=> c(app)
+
+})
